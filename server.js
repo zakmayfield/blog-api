82bcb3e7f7df855b10ae6ee85a672e7a0f6545a5
Zak Mayfield <zakmayfield@gmail.com>
2022-11-17 10:42:18 -0500

    add cors middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/api', require('./routes'))
 
 app.get('/', (req, res) => {
